7d507914a99ca41770859f2d67e6ffb5b9a8bf6b
mohammad sadegh hoseini <m.hoseini5273@outlook.com>
2022-02-01 23:51:51 +0330

    go to display meetups list

---
+++ src/UI/Header.svelte
@@ -0,0 +1,16 @@
+<script>
+
+</script>
+
+<style>
+</style>
+
+<div class="container text-center bg-dark">
+    <div class="row justify-content-center m-2 b">
+        <div class="col-12 ">
+            <h1 class="text-primary">Meeting</h1>
+        </div>
+    </div>
+
+</div>
+
--- src/main.js
+++ src/main.js
@@ -1,7 +1,11 @@
 import App from './App.svelte';
+import Header from "./UI/Header.svelte"
 
 const app = new App({
 	target: document.body
 });
+// const header = new Header({
+// 	target: document.querySelector("#header")
+// })
 
 export default app;
